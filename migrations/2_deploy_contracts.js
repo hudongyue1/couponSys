7b1bb60819db752fda4a0de07261f3f99697dce8
hudongyue <2535593261@qq.com>
2021-12-23 11:47:15 +0800

    change backto webpack

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,4 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
+var Score = artifacts.require("./Score.sol");
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Score);
 };
